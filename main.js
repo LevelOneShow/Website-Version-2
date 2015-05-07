21eb6a32d4aaa7bf475ea1f90c722ad012117db8
MrFlynn <nicholaspleatsikas@gmail.com>
2015-05-06 19:24:39 -0700

    Finished upgrading error analysis and reporting.

--- main.js
+++ main.js
@@ -53,19 +53,47 @@ function toUserJSON(json) {
   })
 }
 
-// -> href, null
-// 
+// -> [function call]
+// Detects the number of errors as set as a sessionStorage key and send either
+// 'true' or 'false' to the function userStepIn depending on the num of errors.
 function sendErrors() {
   console.error("Something went wrong!"); // Reports to the console that something went wrong.
   sessionStorage.crashTimes = (parseInt(sessionStorage.crashTimes) + 1); // Gets crashTimes var from localstorage.
   
   if (isNaN(sessionStorage.crashTimes)) {
     sessionStorage.setItem("crashTimes", 0);
-    // something here.
+    userStepIn(false);
+  } else if (sessionStorage.crashTimes === null) {
+    return null;
   } else if (parseInt(sessionStorage.crashTimes) =< 3) {
-    // something here.
+    userStepIn(false);
   } else {
-    // something here.
+    userStepIn(true);
+  }
+}
+
+// bool -> href 
+// Gives users the choice to either reload the page (up to 3 times), email the
+// developer, or do nothing in the event of an error.
+function userStepIn(multiCrash) {
+  // Confirm dialog box statements:
+  var statement1 = "Whoops! Looks like something went wrong.\nPress \'OK\' to reload the page";
+  var statement2 = "Looks like the issue isn't fixing itself.\nPress \'OK\' to email the developer" + 
+  "to let them know that there is something wrong";
+  
+  switch (multiCrash) {
+    case (true && confirm(statement2)):
+      location.href = "mailto:admin@lvloneshow.com";
+      break;
+    case (true && (!confirm(statement2))):
+      sessionStorage.setItem("crashTimes", null); // Sets the sesion key to 'null' to prevent further dialogs.
+      break;
+    case (false && confirm(statement1)):
+      location.reload();
+      break;
+    default:
+      console.warn("User chose to do nothing...");
+      break;
   }
 }
 
