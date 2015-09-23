a06a41df8aab0c2d098f40f0c192c9883da7d84f
Nick Pleatsikas <nick@pleatsikas.me>
2015-09-22 20:25:59 -0700

    Organized functions and variables and added some descriptions.

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
+# Volta Streaming Backend - Asynchronous Web Server
+# Written by Nick Pleatsikas - pleatsikas.me
+
+# Contact admin@volta.network to report any bugs.
+# View the README for more information.
+
+# Modules: ---------------------------------------------------------------------
+
+# Full Modules:
 import tornado.httpclient
+
+# Partial Modules:
 from tornado import gen
-from tornado.ioloop import IOLoop
+from tornado.ioloop import IOLoop 
 from tornado.web import RequestHandler, Application, url
 
-class MainHandler(tornado.web.RequestHandler):
+# Global Variables: ------------------------------------------------------------
+
+# Port:
+port = 8001
+
+# Functions:  ------------------------------------------------------------------
+
+# Handles all requests to homepage.
+class HomepageHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
     def get(self):
         http = tornado.httpclient.AsyncHTTPClient()
         response = yield http.fetch("http://localhost/test.txt")
         self.write(response.body)
 
+# App Generation: --------------------------------------------------------------
+
+# App Routes & Settings:
 def make_app():
     return Application([
-        (r"/", MainHandler)
+        (r"/", HomepageHandler)
     ])
 
+# Startup: ---------------------------------------------------------------------
+
+# Startup app with these functions and base settings.
 def main():
     app = make_app()
-    app.listen(8080)
+    app.listen(port )
     IOLoop.current().start()
 
+# Init:
 if __name__ == '__main__':
     main()
