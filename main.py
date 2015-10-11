110442900f70e492fb403283103623ad5899b718
MrFlynn <nicholaspleatsikas@gmail.com>
2015-10-10 20:44:55 -0700

    Added more pages.

--- main.py
+++ main.py
@@ -8,7 +8,6 @@
 
 # Full Modules:
 import tornado.httpclient
-import json
 
 # Partial Modules:
 from tornado import gen
@@ -22,9 +21,9 @@ port = 8001
 
 # Functions:  ------------------------------------------------------------------
 
-# prettify_online : List -> DOM Element.
-# ...
-def prettify_online(ary):
+# prettify_streaming : List -> DOM Element.
+# Takes streamer data and turns it into readable DOM elements.
+def prettify_streaming(ary):
     return ary
 template.execute(prettify_online=prettify_online) # Add to template.
 
@@ -33,7 +32,7 @@ class HomepageHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
     def get(self):
         http = tornado.httpclient.AsyncHTTPClient()
-        response = yield http.fetch("http://localhost/test.txt")
+        response = yield http.fetch("http://localhost:9000/api_data.txt")
         self.write(response.body)
         # self.render("home.template.html", streams=response.body) <- Unlock when 
         #                                                                          sample page completed.
@@ -50,7 +49,7 @@ def make_app():
     return Application([
         (r"/", HomepageHandler), # Homepage
         (r"/api", APIHandler), # API
-        (r"/api/enroll", tornado.web.RedirectHandler, dict(url=r"/")), # Enroll API Keys
+        (r"/api/enroll", tornado.web.RedirectHandler, dict(url=r"/")), # Enroll new API Keys
     ])
 
 # Startup: ---------------------------------------------------------------------
@@ -58,7 +57,7 @@ def make_app():
 # Startup app with these functions and base settings.
 def main():
     app = make_app()
-    app.listen(port )
+    app.listen(port)
     IOLoop.current().start()
 
 # Init:
