28b3147fd8c55007bd459b41aa1a9b709f7039f8
MrFlynn <nicholaspleatsikas@gmail.com>
2015-10-15 21:36:43 -0700

    Organized code and finished basic REST API.

--- main.py
+++ main.py
@@ -21,26 +21,33 @@ port = 8001
 
 # Functions:  ------------------------------------------------------------------
 
-# prettify_streaming : List -> DOM Element.
+# query_data : GET -> String
+# Asychronously reads text file on localhost:9000 and outputs the response.
+@tornado.gen.coroutine
+def query_data():
+    http = tornado.httpclient.AsyncHTTPClient()
+    response = yield http.fetch("http://localhost:9000/api_data.txt")
+    return response.body
+
+# prettify_streaming : List -> DOM Element
 # Takes streamer data and turns it into readable DOM elements.
 def prettify_streaming(ary):
     return ary
 template.execute(prettify_online=prettify_online) # Add to template.
 
-# Generates the homepage with active streamers.
-class HomepageHandler(tornado.web.RequestHandler):
-    @tornado.gen.coroutine
-    def get(self):
-        http = tornado.httpclient.AsyncHTTPClient()
-        response = yield http.fetch("http://localhost:9000/api_data.txt")
-        self.write(response.body)
-        # self.render("home.template.html", streams=response.body) <- Unlock when 
-        #                                                                          sample page completed.
+# Pages: -----------------------------------------------------------------------
 
-# Handles requests to the streamer API.
+# /: Generates the homepage with active streamers.
+class HomepageHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(query_data())
+        # self.render("home.template.html", streams=query_data()) <- Unlock when 
+        #                                                            sample page completed.
+
+# /api: Handles requests to the streamer API.
 class APIHanlder(tornado.web.RequestHandler):
     def get(self):
-        self.write("Data coming soon...")
+        self.write(query_data())
 
 # App Generation: --------------------------------------------------------------
 
